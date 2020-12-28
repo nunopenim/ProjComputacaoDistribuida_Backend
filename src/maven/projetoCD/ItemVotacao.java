6e080b0c31d6aeb3a101e1508d3e5e10cbf153d0
nunopenim <nuno.penim@gmail.com>
2020-12-28 20:05:04 +0000

    general: Added nuclear class ItemVotacao

---
+++ src/maven/projetoCD/ItemVotacao.java
@@ -0,0 +1,23 @@
+package maven.projetoCD;
+
+public class ItemVotacao {
+	private String _id;
+	private String _rev = null;
+	private String nome;
+	private int contagem;
+	
+	public ItemVotacao(String id, String nome) {
+		this._id = id;
+		this.nome = nome;
+		this.contagem = 0;
+	}
+	
+	public String toString() {
+		return this._id + "|" + this.nome + "|" + this.contagem;
+	}
+	
+	public void vote() {
+		this.contagem++;
+		this._rev = null;
+	}
+}
