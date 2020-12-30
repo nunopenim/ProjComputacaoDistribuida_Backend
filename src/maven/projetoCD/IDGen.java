53a697849fb9505bd58039ede034b5fa40fb9b53
nunopenim <nuno.penim@gmail.com>
2020-12-30 08:15:33 +0000

    IDGen: updated

--- src/maven/projetoCD/IDGen.java
+++ src/maven/projetoCD/IDGen.java
@@ -3,11 +3,16 @@ package maven.projetoCD;
 public class IDGen {
 	private String _id;
 	private String _rev = null;
-	public int counterValue;
+	private int counterValue;
 	
 	public IDGen(int val) {
 		this._id = "0";
 		this.counterValue = val;
+	}
+	
+	public String idGenerator() {
+		counterValue++;
+		return "" + counterValue;
 	}
 	
 }
