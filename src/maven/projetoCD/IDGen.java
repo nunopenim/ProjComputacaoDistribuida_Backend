9a380913f2a50465c3cf648514af1a7f8ea949af
nunopenim <nuno.penim@gmail.com>
2020-12-30 09:11:52 +0000

    IDGen: added support for user id generator

--- src/maven/projetoCD/IDGen.java
+++ src/maven/projetoCD/IDGen.java
@@ -4,15 +4,23 @@ public class IDGen {
 	private String _id;
 	private String _rev = null;
 	private int counterValue;
+	private int userIDGen;
 	
-	public IDGen(int val) {
+	public IDGen(int val, int userVal) {
 		this._id = "0";
 		this.counterValue = val;
+		this.userIDGen = userVal;
 	}
 	
 	public String idGenerator() {
 		counterValue++;
 		return "" + counterValue;
+	}
+	
+	public String userIDGenerator() {
+		String value = "" + userIDGen;
+		userIDGen--;
+		return value;
 	}
 	
 }
