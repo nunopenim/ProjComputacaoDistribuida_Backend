14e9c76c4068d57f7241355c37bda8eaccc12dbe
nunopenim <nuno.penim@gmail.com>
2020-12-30 09:14:33 +0000

    general: added AuthManager class

---
+++ src/maven/projetoCD/AuthManager.java
@@ -0,0 +1,29 @@
+package maven.projetoCD;
+
+public class AuthManager {
+	static final String defaultBase = "ou=dev,dc=example,dc=com";
+	
+	public boolean validSession(LoginSession session) {
+		return LDAPUtils.validDn(session.getSession());
+	}
+	
+	public boolean authenticate(LoginSession session) {
+		return LDAPUtils.auth(session.getSession(), session.getPW());
+	}
+	
+	public void search() {
+		//TODO
+	}
+	
+	public LoginSession createSession(String user_id, String user_pw, boolean admin) {
+		String userGroup;
+		if (admin) {
+			userGroup = "ou=admin";
+		}
+		else {
+			userGroup = "ou=user";
+		}
+		return new LoginSession(user_id, user_pw, defaultBase, userGroup);
+	}
+	
+}
