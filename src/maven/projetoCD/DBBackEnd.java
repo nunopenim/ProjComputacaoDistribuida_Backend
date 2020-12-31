014225409d521d412b3563511f2615b427d8a7f4
nunopenim <nuno.penim@gmail.com>
2020-12-31 10:19:58 +0000

    general: updated with latest code

--- src/maven/projetoCD/DBBackEnd.java
+++ src/maven/projetoCD/DBBackEnd.java
@@ -1,7 +1,10 @@
 package maven.projetoCD;
 
 /* How stuff works ID-wise:
- * ID: 0 -> Gerador de IDs
+ * ID = 0 			  : Gerador de IDs
+ * ID > 0 			  : Objetos de votação
+ * ID < 0 && ID > -10 : Parametros gerais (Tempo de inicio da votaçao, etc...
+ * ID <= -10		  : Utilizadores que já votaram
  */
 
 public class DBBackEnd {
@@ -41,6 +44,8 @@ public class DBBackEnd {
 		}
 		return false;
 	}
+	
+	
 	
 	public boolean voteInItem(String id) {
 		ItemVotacao item = (ItemVotacao) manager.findObj(ItemVotacao.class, id);
