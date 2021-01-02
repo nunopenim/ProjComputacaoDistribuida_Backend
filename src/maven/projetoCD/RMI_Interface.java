0cb0146f54fe8b78b379dc94c76bfdc1802e89bc
nunopenim <nuno.penim@gmail.com>
2021-01-02 13:53:30 +0000

    general: misc edits, implemented an RMI connection

---
+++ src/maven/projetoCD/RMI_Interface.java
@@ -0,0 +1,10 @@
+package maven.projetoCD;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+//Declaração de funções para o interface RMI
+
+public interface RMI_Interface extends Remote{
+	String testFunc() throws RemoteException;
+}
